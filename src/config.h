bf47e7e2ed529d6dd7f4c629c18d03583529893c
Robbe Louwet <robbe.louwet@student.kuleuven.be>
2023-06-02 15:47:23 +0200

    idk

--- src/config.h
+++ src/config.h
@@ -7,8 +7,8 @@
 
 // @formatter:off
 // -- CONFIG --
-#define slice_size 512 // 8, 32, 64, 128, 256 or 512
-#define ENABLE_SEGMENTATION true
+#define slice_size 64 // 8, 32, 64, 128, 256 or 512
+#define ENABLE_SEGMENTATION false
 #define IMPROVED_KEYSCHEDULE true
 #define FAST_SLICING true
 // ------------
